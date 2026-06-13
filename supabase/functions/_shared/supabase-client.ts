d05e41fc18d1cd41a37652b6249be0047879506a
Azaiah00 <azaiahwood@gmail.com>
2026-06-13 14:55:32 -0400

    Add files via upload

--- supabase/functions/_shared/supabase-client.ts
+++ supabase/functions/_shared/supabase-client.ts
@@ -6,4 +6,6 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 const SUPABASE_URL = Deno.env.get('SUPABASE_URL')!;
 const SUPABASE_SERVICE_KEY = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')!;
 
-export const supabaseAdmin = createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY);
+export const supabaseAdmin = createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY, {
+  db: { schema: 'bng' },
+});
